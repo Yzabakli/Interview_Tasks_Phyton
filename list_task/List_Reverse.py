fd30f658bbf440fc2771f1fe73a2819075e75d4b
Yzabakli <abakliyusufziya@gmail.com>
2022-08-27 17:03:23 +0200

    Reverse task added

--- list_task/List_Reverse.py
+++ list_task/List_Reverse.py
@@ -1,7 +1,15 @@
 def _reverse(array):
 
-    array.reverse()
-    return array
+    _list = []
+
+    i = len(array) - 1
+
+    while i >= 0:
+
+        _list.append(array[i])
+        i -= 1
+
+    return _list
 
 
 print(_reverse([1, 2, 3, 4, 5]))
