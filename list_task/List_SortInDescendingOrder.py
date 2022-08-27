4cae67875d08f0d232f96f62a8764f0c4125514f
Yzabakli <abakliyusufziya@gmail.com>
2022-08-27 17:12:48 +0200

    Sort in Descending Order

---
+++ list_task/List_SortInDescendingOrder.py
@@ -0,0 +1,12 @@
+def _sort_in_descending_order(array):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+
+            if array[j] > array[i]:
+                temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+    return array
+
+
+print(_sort_in_descending_order([5, 5, 5, 5, 5, 4, 3, 2, 1, 5]))
